0829838c3887f53242c19332a1872bb44f8b5a7c
Ritesh Verma <2k24.cs1m.2413434@gmail.com>
2026-08-30 18:01:38 +0530

    Refactor minimumDeletions function for clarity_ removing redundancy

--- Arrays/2091_Removing Minimum and Maximum From Array.cpp
+++ Arrays/2091_Removing Minimum and Maximum From Array.cpp
@@ -28,3 +28,32 @@ public:
         
     }
 };
+
+
+
+// After Removing the Redundancy from my code 
+class Solution {
+public:
+    int minimumDeletions(vector<int>& nums) {
+        int n = nums.size();
+        int index1 = 0;
+        int index2 = 0;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[index1])
+                index1 = i;
+
+            if (nums[i] > nums[index2])
+                index2 = i;
+        }
+        int left = min(index1, index2);
+        int right = max(index1, index2);
+
+        int ans1 = right + 1;
+        int ans2 = (left + 1) + (n - right);
+        int ans3 = n - left;
+
+        return ans1 < ans2 && ans1 < ans3 ? ans1 : ans2 < ans3 ? ans2 : ans3;  
+
+        
+    }
+};
