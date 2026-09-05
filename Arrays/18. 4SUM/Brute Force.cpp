516e01bfd4385c7b7d878c11c324f38296b0368e
Ritesh Verma <2k24.cs1m.2413434@gmail.com>
2026-09-05 22:38:20 +0530

    Implement brute force solution for 4SUM problem with  tc - O(n^4)

---
+++ Arrays/18. 4SUM/Brute Force.cpp
@@ -0,0 +1,23 @@
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        set<vector<int>> st;
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1; j < n; j++){
+                for(int k = j + 1; k < n; k++){
+                    for(int l = k + 1; l < n; l++){
+                        if(nums[i] + nums[j] + nums[k] + nums[l] == target){
+                            vector<int> temp = { nums[i], nums[j], nums[k], nums[l]};
+                            sort(temp.begin(), temp.end());
+                            st.insert(temp);
+                        }
+                    }
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
