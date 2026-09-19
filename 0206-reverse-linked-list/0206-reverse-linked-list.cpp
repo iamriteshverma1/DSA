9eb9986244fca1de78a44c9fefc8d145930a5cb8
Ritesh Verma <2k24.cs1m.2413434@gmail.com>
2026-09-20 01:07:34 +0530

    Time: 0 ms (100%), Space: 13.7 MB (7.65%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -34,7 +34,7 @@ public:
 };
 
 */
-
+/*
 class Solution {
 public :
     ListNode* reverseList(ListNode* head) {
@@ -55,5 +55,23 @@ public :
         }
         head = prev;
         return head;
+    }
+};
+
+*/
+
+class Solution {
+public :
+    ListNode* reverseList(ListNode* head) {
+
+        if(head == NULL || head -> next == NULL)
+            return head;
+
+        ListNode* newHead = reverseList(head -> next);
+        ListNode* front = head -> next;
+        front -> next = head;
+        head -> next = NULL;
+
+        return newHead;
     }
 };
