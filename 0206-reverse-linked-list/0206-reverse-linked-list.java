aa059be19ddf64043df7abef55376980a48c864d
Ritesh Verma <2k24.cs1m.2413434@gmail.com>
2026-09-20 01:04:39 +0530

    Time: 0 ms (100%), Space: 44 MB (92.15%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode temp = head;
@@ -24,3 +26,21 @@ class Solution {
         return prev;
     }
 }
+
+*/
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        if(head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode newHead = reverseList(head.next);
+
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+};
